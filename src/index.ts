b36424b26773000ec619005af0abaccdf793a175
Tommaso Prandin <tommy.prando@gmail.com>
2021-07-02 12:17:57 +0200

    removed node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import faker, { random } from "faker";
+import fs from "fs"
 
 faker.setLocale("it")
 dayjs.locale("it")
+
+if (fs.existsSync('note-spesa.csv'))
+    fs.writeFileSync('note-spesa.csv', '')
 
 interface Cliente {
     nome: string,
@@ -11,7 +15,7 @@ interface Cliente {
 }
 
 interface Viaggio {
-    data: string,
+    data: Dayjs,
     co: string,
     partenza: string,
     destinazione: string
@@ -31,36 +35,80 @@ const clienti: Cliente[] = [
         indirizzo: "Via Leonardo Da Vinci, 20, Brugine (PD)",
         distanza: 11.3
     },
-    {
-        nome: "Tri.Lem Srl Logistica e trasporti",
-        indirizzo: "Via dell'Artigianato, 13, Grantorto (PD)",
-        distanza: 47.6
-    }
 ]
 
-const sogliaMensile = 100
-
-let sommaKm = 0
-let giorni = 0
-
-while (sommaKm < sogliaMensile) {
-    const data = faker.date.soon(5, dayjs().add(giorni).toDate())
-    if (data.getDay() === 5 || data.getDay() === 6)
-        continue
-    
-    const cliente = random.arrayElement<Cliente>(clienti)
-
-    const viaggio: Viaggio = {
-        co: cliente.nome,
-        partenza: casa,
-        destinazione: cliente.indirizzo,
-        km: cliente.distanza * 2,
-        data: dayjs(data).toString()
-    }
-
-    sommaKm += viaggio.km
-    giorni++
-
-    console.log(viaggio);    
-
+const lavoro: Cliente = {
+    nome: "Tri.Lem Srl Logistica e trasporti",
+    indirizzo: "Via dell'Artigianato, 13, Grantorto (PD)",
+    distanza: 47.6
 }
+
+const sogliaMensile = 3295
+const anno = 2020
+
+const viaggi: Viaggio[] = []
+
+// ciclo annuale
+for (let mese = 0; mese < 12; mese++) {
+
+    let sommaKmMese = 0
+
+    // ciclo mensile
+    for (let giorno = 1; giorno < dayjs().year(anno).month(mese).daysInMonth(); giorno++) {
+
+        // console.log(`mese: ${dayjs().month(mese).format("MMM")} somma km mese: ${sommaKmMese}, soglia: ${sogliaMensile}`);
+        
+        if (sommaKmMese >= sogliaMensile)
+            break
+
+        const data = dayjs().year(anno).month(mese).add(giorno, 'days')
+
+        if (data.day() === 0 || data.day() === 6)
+                continue
+
+        // viaggio Trilem ogni giorno
+        viaggi.push({
+            co: lavoro.nome,
+            data,
+            partenza: casa,
+            destinazione: lavoro.indirizzo,
+            km: lavoro.distanza * 2
+        })
+
+        sommaKmMese += lavoro.distanza * 2
+
+        // numero random di viaggi nel giorno
+        const numeroViaggi = faker.datatype.number() % 8
+
+        for (let i = 1; i <= numeroViaggi; i++) {
+        
+            const cliente = random.arrayElement<Cliente>(clienti)
+
+            viaggi.push({
+                co: cliente.nome,
+                partenza: casa,
+                destinazione: cliente.indirizzo,
+                km: cliente.distanza * 2,
+                data
+            })
+
+            sommaKmMese += cliente.distanza * 2
+        } 
+
+    }
+}
+
+viaggi.sort((viaggio1, viaggio2) => {
+    if (viaggio1.data.isSame(viaggio2.data, 'day'))
+        return 0
+    return viaggio1.data.isBefore(viaggio2.data) ? -1 : 1
+})
+
+viaggi.forEach(viaggio => {
+    fs.appendFileSync(
+        'note-spesa.csv',
+        `"${viaggio.data.format("DD/MM/YYYY")}", "${viaggio.co}", "${viaggio.partenza}", "${viaggio.destinazione}", "${viaggio.destinazione}", "${viaggio.partenza}", "${viaggio.km.toString().replace(".", ",")}"\n`,
+    )
+})
+
+console.log("Finito!");
